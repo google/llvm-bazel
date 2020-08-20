84fb7ca93925bca31d864d722190897afcb636f2
Geoffrey Martin-Noble <gcmn@google.com>
2020-08-20 09:41:58 -0700

    fixup! Add script for performing a fake git merge that clobbers changes

--- scripts/clobber_merge.sh
+++ scripts/clobber_merge.sh
@@ -4,6 +4,10 @@
 # See https://llvm.org/LICENSE.txt for license information.
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+# Creates a fake merge between the specified branches where the current state of
+# the second parent (default "main") overrides the state of the first parent
+# (default the current branch).
+
 FIRST_PARENT="${1:-$(git rev-parse --abbrev-ref HEAD)}"
 SECOND_PARENT="${2:-main}"
 
