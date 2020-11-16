google/llvm-bazel

commit c42f29eaab657c9e91e0a655e832e8804d234720

Clean-up and remove hard-coded flag from `tblgen` Bazel rule. (#71)
The hard coded flag disables the use of long string literals. This isn't
supported for all tablegen tools, and isn't desirable in any of them. It
is a workaround for compilers that can't handle the long string
literals. For compilers that can handle these literals, using them is
a (significant) compile time improvement.

I don't believe any compilers we're supporting with Bazel currently
require this flag, and if they do we should find out and pass it very
narrowly for those compilers. First step is to remove it here.

I've tested this both on a recent Clang and GCC 5 successfully.

llvm-bazel/llvm-project-overlay/llvm/tblgen.bzl
@@ -40,10 +40,10 @@ def gentbl(name, tblgen, td_file, td_srcs, tbl_outs, library = True, **kwargs):
             outs = [out],
             tools = [tblgen],
             message = "Generating code from table: %s" % td_file,
-            cmd = (("$(location %s) " + "-I external/llvm-project/llvm/include " +
+            cmd = (("$(location %s) -I external/llvm-project/llvm/include " +
                     "-I external/llvm-project/clang/include " +
-                    "-I $$(dirname $(location %s)) " + ("%s $(location %s) --long-string-literals=0 " +
-                                                        "-o $@")) % (
+                    "-I $$(dirname $(location %s)) " +
+                    "%s $(location %s) -o $@") % (
                 tblgen,
                 td_file,
                 opts,

commit d2579a8a9040ffafcc8342a1fde3a4ec496b11e1

Set --long-string-literals=0 on Windows in tblgen.bzl again. (#85)
This fixes a build failure on MSVC introduced by https://github.com/google/llvm-bazel/pull/71. See https://github.com/google/llvm-bazel/pull/71#issuecomment-724276137 and https://github.com/google/iree/issues/3756 for context.

llvm-bazel/llvm-project-overlay/llvm/BUILD
@@ -1658,6 +1658,13 @@ gentbl(
         strip_include_prefix = "lib/Target/" + target["name"],
         tbl_outs = target["tbl_outs"],
         tblgen = ":llvm-tblgen",
+        # MSVC isn't happy with long string literals, while other compilers
+        # which support them get significant compile time improvements with
+        # them enabled. Ideally this flag would only be enabled on Windows via
+        # a select() on `@bazel_tools//src/conditions:windows,`, but that would
+        # require refactoring gentbl from a macro into a rule.
+        # TODO(#92): Refactor gentbl to support this use
+        tblgen_args = "--long-string-literals=0",
         td_file = "lib/Target/" + target["name"] + "/" + target["short_name"] + ".td",
         td_srcs = [
             ":common_target_td_sources",

llvm-bazel/llvm-project-overlay/llvm/tblgen.bzl
@@ -12,7 +12,15 @@ TODO(chandlerc): Currently this expresses include-based dependencies as
 correctly understood by the build system.
 """
 
-def gentbl(name, tblgen, td_file, td_srcs, tbl_outs, library = True, **kwargs):
+def gentbl(
+        name,
+        tblgen,
+        td_file,
+        td_srcs,
+        tbl_outs,
+        library = True,
+        tblgen_args = "",
+        **kwargs):
     """gentbl() generates tabular code from a table definition file.
 
     Args:
@@ -24,6 +32,7 @@ def gentbl(name, tblgen, td_file, td_srcs, tbl_outs, library = True, **kwargs):
         options passed to tblgen, and the out is the corresponding output file
         produced.
       library: Whether to bundle the generated files into a library.
+      tblgen_args: Extra arguments string to pass to the tblgen binary.
       **kwargs: Keyword arguments to pass to subsidiary cc_library() rule.
     """
     if td_file not in td_srcs:
@@ -39,11 +48,12 @@ def gentbl(name, tblgen, td_file, td_srcs, tbl_outs, library = True, **kwargs):
             cmd = (("$(location %s) -I external/llvm-project/llvm/include " +
                     "-I external/llvm-project/clang/include " +
                     "-I $$(dirname $(location %s)) " +
-                    "%s $(location %s) -o $@") % (
+                    "%s $(location %s) %s -o $@") % (
                 tblgen,
                 td_file,
                 opts,
                 td_file,
+                tblgen_args,
             )),
         )
 